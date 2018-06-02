d645e380676f2b081099a3f2bd7ab3c5feb6f8cc
00010216 <00010216@uca.edu.sv>
2018-06-02 14:47:54 -0600

    se creo la actividad especifica para levantar el login y se quito del main

--- app/src/main/java/com/example/kcruz/gamenews/LogInActivity.java
+++ app/src/main/java/com/example/kcruz/gamenews/LogInActivity.java
@@ -1,4 +1,12 @@
 package com.example.kcruz.gamenews;
 
-public class LogInActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LogInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
