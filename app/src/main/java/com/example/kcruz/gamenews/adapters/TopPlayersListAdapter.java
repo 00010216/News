7536837d5c6f7c8630ddc53c6c51a553df5e4989
00010216 <00010216@uca.edu.sv>
2018-06-04 12:08:12 -0600

    se creo el adapter que administrara la info para los item de la topplayer list

--- app/src/main/java/com/example/kcruz/gamenews/adapters/TopPlayersListAdapter.java
+++ app/src/main/java/com/example/kcruz/gamenews/adapters/TopPlayersListAdapter.java
@@ -1,4 +1,62 @@
 package com.example.kcruz.gamenews.adapters;
 
-public class TopPlayersListAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.kcruz.gamenews.R;
+import com.example.kcruz.gamenews.models.Player;
+import com.example.kcruz.gamenews.models.TopPlayer;
+
+import java.util.List;
+
+public class TopPlayersListAdapter extends RecyclerView.Adapter<TopPlayersListAdapter.TopPlayersViewHolder>{
+    private Context context;
+    List<TopPlayer> topPlayers; //declarando lista que contendra todos los topplayers
+
+    //constructor que recibira la lista y el contexto desde el fragmento TopPlayerListFragment
+    public TopPlayersListAdapter(Context context, List<TopPlayer> topPlayerList) {
+        this.context = context;
+        this.topPlayers = topPlayerList;
+    }
+
+
+
+    public static class TopPlayersViewHolder extends RecyclerView.ViewHolder {
+        //indica la vista donde se ubicaran la informacion de la lista
+        ImageView img;
+        TextView title,subtitle;
+
+        public TopPlayersViewHolder(View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.top_player_image);
+            title = itemView.findViewById(R.id.top_player_name);
+            subtitle = itemView.findViewById(R.id.top_player_subtitle);
+        }
+    }
+
+    @Override
+    public TopPlayersViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //se infla la vista del item
+        View view = LayoutInflater.from(context).inflate(R.layout.top_player_item,parent,false);
+        return (new TopPlayersViewHolder(view));
+    }
+
+    @Override
+    public void onBindViewHolder(TopPlayersViewHolder holder, int position) {
+        //se asignar el contenido a cada view que contiene el item
+        holder.img.setImageResource(topPlayers.get(position).getImage());
+        holder.title.setText(topPlayers.get(position).getName());
+        holder.subtitle.setText(topPlayers.get(position).getSubtitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return topPlayers.size();
+    }
+
 }
