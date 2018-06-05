18226bbb36c5e8c5cc869602f139979bb0daeb3d
00010216 <00010216@uca.edu.sv>
2018-06-04 21:32:33 -0600

    se creo la interfaz que contienen el link del API y se definio el metodo POST para verificar el usuario

--- app/src/main/java/com/example/kcruz/gamenews/API/GameNewsAPI.java
+++ app/src/main/java/com/example/kcruz/gamenews/API/GameNewsAPI.java
@@ -1,4 +1,23 @@
 package com.example.kcruz.gamenews.API;
 
+import com.example.kcruz.gamenews.models.User;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 public interface GameNewsAPI {
+
+    String ENDPOINT = "http://gamenewsuca.herokuapp.com/";
+
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<String> token(@Field("user") String user, @Field("password") String password);
+
 }
