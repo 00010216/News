a0adbcaebbaad4eb747ee392faa502de1007ce4d
00010216 <00010216@uca.edu.sv>
2018-06-04 21:31:10 -0600

    se convierte el json a un string en este caso para el token

--- app/src/main/java/com/example/kcruz/gamenews/API/TokenDeserializer.java
+++ app/src/main/java/com/example/kcruz/gamenews/API/TokenDeserializer.java
@@ -1,4 +1,23 @@
 package com.example.kcruz.gamenews.API;
 
-public class TokenDeserializer {
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+
+public class TokenDeserializer implements JsonDeserializer<String> {
+
+    @Override
+    public String deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        String logInToken = "";
+
+        if(json.getAsJsonObject()!= null){
+            JsonObject jsonObject = json.getAsJsonObject(); //obtenemos el jason del api sabiendo que no es nulo
+            logInToken = jsonObject.get("token").getAsString();
+        }
+        return logInToken;
+    }
 }
