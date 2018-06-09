300b72cca81b258fb10c9f3172942a5206135a4a
00010216 <00010216@uca.edu.sv>
2018-06-08 19:06:14 -0600

    se creo el fragmento para administrar noticias

--- app/src/main/java/com/example/kcruz/gamenews/fragments/NewsFragment.java
+++ app/src/main/java/com/example/kcruz/gamenews/fragments/NewsFragment.java
@@ -1,4 +1,28 @@
 package com.example.kcruz.gamenews.fragments;
 
-public class NewsFragment {
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.kcruz.gamenews.adapters.ImagesAdapter;
+import com.example.kcruz.gamenews.adapters.NewsAdapter;
+import com.example.kcruz.gamenews.models.Image;
+import com.example.kcruz.gamenews.models.News;
+
+import java.util.List;
+
+public class NewsFragment extends Fragment {
+
+    public static final String ARG_ITEM_ID = "tab_images_grid";
+    Activity activity;
+    NewsAdapter newsAdapter;
+    RecyclerView newsView;
+    GridLayoutManager lManager;
+    List<News> news;
+
+    public NewsFragment() {
+    }
+
+
 }
