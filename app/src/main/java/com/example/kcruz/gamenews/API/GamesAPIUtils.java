7b24c5ea0b0dcf1b35ea4c575b36bea9980bf360
00010216 <00010216@uca.edu.sv>
2018-06-14 15:17:23 -0600

    esta clase administrar las peticiones a la API y procesa las respuestas

--- app/src/main/java/com/example/kcruz/gamenews/API/GamesAPIUtils.java
+++ app/src/main/java/com/example/kcruz/gamenews/API/GamesAPIUtils.java
@@ -1,4 +1,58 @@
 package com.example.kcruz.gamenews.API;
 
+import com.example.kcruz.gamenews.utils.GameNewsSharedPreferences;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class GamesAPIUtils {
+    //pueden ser instanciados sin necesidad de crear un objeto
+    private static Retrofit retrofit;
+    private static String ENDPOINT = "http://gamenewsuca.herokuapp.com/";
+    private static GameNewsAPI gameNewsAPI;
+
+    public static GameNewsAPI getApiInstance(){
+        //instancia para llamar la API y manejar la informacion
+        retrofit = new Retrofit.Builder()
+                .baseUrl(ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        gameNewsAPI = retrofit.create(GameNewsAPI.class);
+        return gameNewsAPI;
+    }
+
+    public static GameNewsAPI getApiInstanceWithAuthorization() {
+        //cuando ocupa el POJO no necesita de un gson
+        return getApiInstanceWithAuthorization(new GsonBuilder().create());
+    }
+
+    public static GameNewsAPI getApiInstanceWithAuthorization(Gson gson) {
+        //cuando ocupa el POJO no necesita de un gson
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request newRequest = chain.request()
+                                .newBuilder()
+                                .addHeader("Authorization", "Bearer " + GameNewsSharedPreferences.getToken())
+                                .build();
+                        return chain.proceed(newRequest);
+                    }
+                }).build();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(ENDPOINT)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        gameNewsAPI = retrofit.create(GameNewsAPI.class);
+        return gameNewsAPI;
+    }
 }
