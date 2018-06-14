a01b3261e77b17fe96aebdb9a409773c478276fb
00010216 <00010216@uca.edu.sv>
2018-06-13 21:24:09 -0600

    se creo una clase login para manejar el token con los nuevos cambios hechos en el API

---
+++ app/src/main/java/com/example/kcruz/gamenews/API/Login.java
@@ -0,0 +1,14 @@
+package com.example.kcruz.gamenews.API;
+
+public class Login {
+    private String token;
+    private String message;
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
