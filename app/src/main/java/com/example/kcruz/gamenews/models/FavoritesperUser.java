1358e72c6fcc60c3144b31a1da19311263cec039
00010216 <00010216@uca.edu.sv>
2018-06-15 20:26:33 -0600

    se creo el modelo para trabajar los favoritos

--- app/src/main/java/com/example/kcruz/gamenews/models/FavoritesperUser.java
+++ app/src/main/java/com/example/kcruz/gamenews/models/FavoritesperUser.java
@@ -1,4 +1,31 @@
 package com.example.kcruz.gamenews.models;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "favorites_per_user",primaryKeys = {"news_id","news_id"},
+        foreignKeys = {
+                @ForeignKey
+                        (entity = User.class,
+                                parentColumns = "_id",
+                                childColumns = "user_id",
+                                onDelete = ForeignKey.CASCADE,
+                                onUpdate = ForeignKey.CASCADE),
+                @ForeignKey
+                        (entity = User.class,
+                                parentColumns = "_id",
+                                childColumns = "news_id",
+                                onDelete = ForeignKey.CASCADE,
+                                onUpdate = ForeignKey.CASCADE)})
 public class FavoritesperUser {
+    @NonNull
+    private int user_id;
+    @NonNull
+    private int news_id;
+
+    public FavoritesperUser(@NonNull int news_id, @NonNull int user_id) {
+        this.news_id = news_id;
+        this.user_id = user_id;
+    }
 }
